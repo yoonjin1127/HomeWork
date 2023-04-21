d8d6deae20c069a5ccb2034cdb03c1159f52a6b5
윤진 소 <pms010506@gmail.com>
2023-04-21 19:45:06 +0900

    초기 커밋.

---
+++ 0421.Stack/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _0421.Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
